3a2db39b52e796a2e40ee4ec33aa54dc25dd6f1a
temcray <ejamison06@gmail.com>
2025-09-14 14:40:04 -0500

    changes

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
-class HomePagewView(TemplateView):
+class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 class AboutPageView(TemplateView):
-    template_name = "about.html"
+    template_name = "pages/about.html"
 
